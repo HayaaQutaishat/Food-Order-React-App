6b6af3a60456d6398d949aa83b54c7f23d457808
“Hayaa”Qutaishat” <hayaa.qutaishat@gmail.com>
2022-11-04 14:38:19 +0100

    Working with Refs & Forward Refs

--- src/components/Meals/MealItem.js
+++ src/components/Meals/MealItem.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+import CartContext from "../../store/cart-context";
 import classes from "./MealItem.module.css";
 import MealItemForm from "./MealItemForm";
 
 const MealItem = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const addtoCartHandler = (amount) => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      price: props.price,
+      amount: amount,
+    });
+  };
+
   return (
     <li className={classes.meal}>
       <div>
@@ -11,7 +23,7 @@ const MealItem = (props) => {
         <div className={classes.price}>${props.price.toFixed(2)}</div>
       </div>
       <div>
-        <MealItemForm />
+        <MealItemForm onAddToCart={addtoCartHandler} />
       </div>
     </li>
   );
--- src/components/Meals/MealItemForm.js
+++ src/components/Meals/MealItemForm.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import classes from "./MealItemForm.module.css";
 import Input from "../UI/Input";
 
 const MealItemForm = (props) => {
+  const amountInputRef = useRef();
+  const [formIsValid, setFormIsValid] = useState(true);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 0 ||
+      enteredAmountNumber > 5
+    ) {
+      setFormIsValid(false);
+      return;
+    }
+    props.onAddToCart(enteredAmountNumber);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
           id: "amount",
@@ -17,6 +36,7 @@ const MealItemForm = (props) => {
         }}
       />
       <button>+Add</button>
+      {!formIsValid && <p>Please enter a valid amount (1-5).</p>}
     </form>
   );
 };
