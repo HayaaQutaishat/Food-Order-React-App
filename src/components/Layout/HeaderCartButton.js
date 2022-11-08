7222eeac36e47f855e970c1a459a34f9b7a35a8c
“Hayaa”Qutaishat” <hayaa.qutaishat@gmail.com>
2022-11-08 16:50:42 +0100

    Adding Animation Using the useEffect Hook

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,38 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(CartContext);
+  const [isHighlighted, setIsHighlighted] = useState(false);
 
   const numberOfCartItems = cartCtx.items.reduce((currentNum, item) => {
     return currentNum + item.amount;
   }, 0);
 
+  const { items } = cartCtx;
+  const btnClasses = `${classes.button} ${isHighlighted ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setIsHighlighted(true);
+
+    // remove the class
+    const timer = setTimeout(() => {
+      setIsHighlighted(false);
+    }, 300);
+
+    // cleanup function
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onShowCart}>
+    <button className={btnClasses} onClick={props.onShowCart}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
