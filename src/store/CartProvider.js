1ff9c8f19110942c21e15b5b4772c70a3f113041
“Hayaa”Qutaishat” <hayaa.qutaishat@gmail.com>
2022-11-08 14:54:30 +0100

    The logic of removing items from the cart

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -35,6 +35,28 @@ const cartReducer = (state, action) => {
     };
   }
   if (action.type === "REMOVE") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingCartItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingCartItem.price;
+    let updatedItems;
+    if (existingCartItem.amount === 1) {
+      // remove the item from the cart
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      // don't remove the item from the array (just update the amount)
+      const updatedItem = {
+        ...state.item,
+        amount: existingCartItem.amount - 1,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
   }
   return defaultCartState;
 };
