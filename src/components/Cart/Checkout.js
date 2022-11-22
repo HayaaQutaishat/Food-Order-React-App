d5b2949c8739275fa46b4c61d30a1e8806da8012
“Hayaa”Qutaishat” <hayaa.qutaishat@gmail.com>
2022-11-22 16:07:15 +0100

    Reading Form Values

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,31 +1,42 @@
+import { useRef } from "react";
 import classes from "./Checkout.module.css";
-
 const Checkout = (props) => {
+  const nameInputRef = useRef();
+  const streetInputRef = useRef();
+  const portalInputRef = useRef();
+  const cityInputRef = useRef();
+
   const confirmCheckoutHandler = (event) => {
     event.preventDefault();
+    const enteredName = nameInputRef.current.value;
+    const enteredStreet = streetInputRef.current.value;
+    const enteredPostal = portalInputRef.current.value;
+    const enteredCity = cityInputRef.current.value;
   };
   return (
-    <form onSubmit={confirmCheckoutHandler}>
+    <form className={classes.form} onSubmit={confirmCheckoutHandler}>
       <div className={classes.control}>
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" />
+        <input type="text" id="name" ref={nameInputRef} />
       </div>
       <div className={classes.control}>
         <label htmlFor="street">Street</label>
-        <input type="text" id="street" />
+        <input type="text" id="street" ref={streetInputRef} />
       </div>
       <div className={classes.control}>
         <label htmlFor="postal">Postal Code</label>
-        <input type="text" id="postal" />
+        <input type="text" id="postal" ref={portalInputRef} />
       </div>
       <div className={classes.control}>
         <label htmlFor="city">City</label>
-        <input type="text" id="city" />
+        <input type="text" id="city" ref={cityInputRef} />
       </div>
-      <button type="button" onClick={props.onHideCart}>
-        Cancel
-      </button>
-      <button>Confirm</button>
+      <div className={classes.actions}>
+        <button type="button" onClick={props.onHideCart}>
+          Cancel
+        </button>
+        <button className={classes.submit}>Confirm</button>
+      </div>
     </form>
   );
 };
