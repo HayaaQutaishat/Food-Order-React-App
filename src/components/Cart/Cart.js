5094aca286dfd65818e43ca50bef906497c7e975
“Hayaa”Qutaishat” <hayaa.qutaishat@gmail.com>
2022-11-23 09:55:23 +0100

    Adding Better User Feedback

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -7,9 +7,12 @@ import Checkout from "./Checkout";
 
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
+  const [isSubmiting, setIsSubmiting] = useState(false);
+  const [didSubmit, setDidSubmit] = useState(false);
 
-  const SendDataToBackendHandler = (userData) => {
-    fetch(
+  const SendDataToBackendHandler = async (userData) => {
+    setIsSubmiting(true);
+    await fetch(
       "https://react-http-489ae-default-rtdb.europe-west1.firebasedatabase.app/order.json",
       {
         method: "POST",
@@ -19,6 +22,9 @@ const Cart = (props) => {
         }),
       }
     );
+    setIsSubmiting(false);
+    setDidSubmit(true);
+    cartCtx.clearCart();
   };
 
   const [showCheckoutForm, setShowCheckoutForm] = useState(false);
@@ -51,8 +57,8 @@ const Cart = (props) => {
     </ul>
   );
 
-  return (
-    <Modal onHideCart={props.onHideCart}>
+  const cartModalContent = (
+    <React.Fragment>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
@@ -76,6 +82,26 @@ const Cart = (props) => {
           </button>
         )}
       </div>
+    </React.Fragment>
+  );
+
+  const isSubmitingContent = <p>Sending Order data...</p>;
+  const doneSubmitContent = (
+    <React.Fragment>
+      <p>Successfully sent the order!</p>
+      <div className={classes.actions}>
+        <button className={classes.button} onClick={props.onHideCart}>
+          Close
+        </button>
+      </div>
+    </React.Fragment>
+  );
+
+  return (
+    <Modal onHideCart={props.onHideCart}>
+      {!isSubmiting && !didSubmit && cartModalContent}
+      {isSubmiting && isSubmitingContent}
+      {!isSubmiting && didSubmit && doneSubmitContent}
     </Modal>
   );
 };
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -58,6 +58,10 @@ const cartReducer = (state, action) => {
       totalAmount: updatedTotalAmount,
     };
   }
+
+  if (action.type === "CLEAR") {
+    return defaultCartState;
+  }
   return defaultCartState;
 };
 
@@ -72,11 +76,16 @@ const CartProvider = (props) => {
     dispatchCart({ type: "REMOVE", id: id });
   };
 
+  const clearCartHandler = () => {
+    dispatchCart({ type: "CLEAR" });
+  };
+
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
+    clearCart: clearCartHandler,
   };
   return (
     <CartContext.Provider value={cartContext}>
